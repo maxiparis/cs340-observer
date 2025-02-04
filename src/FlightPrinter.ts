00aa2f1dd07dc59f0fb867bfd8bb6f554b93726e
Maximiliano Paris <mparis@hotmail.cl>
2025-02-03 22:29:17 -0700

    feat: printer

--- src/FlightPrinter.ts
+++ src/FlightPrinter.ts
@@ -3,6 +3,6 @@ import { Flight } from "./entity/Flight";
 
 export default class FlightPrinter implements Observer {
     update(flight: Flight): void {
-        console.log(flight)
+        console.log(`✈️Flight update: flight’s transponder id (icao24): ${flight.icao24}, call sign: ${flight.callsign}, country of origin: ${flight.origin_country}, longitude: ${flight.longitude}, latitude: ${flight.latitude}, velocity: ${flight.velocity}, altitude: ${flight.baro_altitude}.`)
     }
 }
